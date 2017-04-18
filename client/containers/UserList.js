ca87bbc2f5be77864c051580f8cdbfaf86429b49
Brian Fajardo <bmf5@hotmail.com>
2017-04-18 16:50:40 -0400

    Add styling

--- client/containers/UserList.js
+++ client/containers/UserList.js
@@ -2,27 +2,35 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import * as actions from '../actions'
+import * as styles from '../../styles/styles'
 
 class UserList extends Component {
-  // Load initial dummy data on mount and
-  // re-render
+  // Load initial dummy data on mount and re-render
   componentWillMount() {
     this.props.fetchUsers()
   }
 
   renderUser(user) {
     return (
-      <div className="card card-block">
-        <h4 className="card-title">{user.name}</h4>
-        <p className="card-text">Twitch.tv</p>
-        <a className="btn btn-info btn-sm">Email</a>
+      <div
+        className="card"
+        style={styles.card}
+      >
+        <div className="card-block">
+          <h4 className="card-title">{user.name}</h4>
+          <p className="card-text">Twitch.tv</p>
+          <a className="btn btn-outline-info btn-sm">Email</a>
+        </div>
       </div>
     )
   }
 
   render() {
     return (
-      <div>
+      <div
+        className="user-list"
+        style={styles.userList}
+      >
         {this.props.users.map(this.renderUser)}
       </div>
     )
---
+++ styles/styles.js
@@ -0,0 +1,10 @@
+export const userList = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexWrap: 'wrap',
+}
+
+export const card = {
+  width: '30%',
+  minWidth: '300px'
+}
